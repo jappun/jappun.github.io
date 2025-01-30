e21e15d47e49a9dbfa60c55be71f608235861898
jappun <jappun@student.ubc.ca>
2025-01-30 01:20:35 -0800

    Added picture of me :)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,39 @@
+// const Home = () => {
+//     return (
+//     <div className="flex flex-col font-mont items-center justify-center h-screen">
+//         <div className="text-primary w-full text-7xl font-extrabold text-center">
+//             Hi, I'm Jappun.
+//         </div>
+//         <div className="max-w-screen flex justify-center text-lg text-primary mt-4 mx-4">
+//             Building software. Telling stories. Finding the narratives in both.
+//         </div>
+//     </div>
+// );};
+import picture from "../img/london.jpg"
 const Home = () => {
     return (
-    <div className="flex flex-col font-mont items-center justify-center h-screen">
-        <div className="text-primary w-full text-7xl font-extrabold text-center">
-            Hi, I'm Jappun.
+        <div className="flex flex-col md:flex-row font-mont items-center justify-center h-screen">
+        
+
+            <div className="flex flex-col">
+                <div className=" text-primary w-full text-7xl font-extrabold text-center mt-4">
+                    Hi, I'm Jappun.
+                </div>
+                <div className="max-w-screen flex justify-center text-lg text-center text-primary mt-4 mx-4">
+                    Building software. Telling stories. Finding the narratives in both.
+                </div>
+            </div>
+
+            <img 
+                src={picture}
+                alt="Jappun's Pic" 
+                className="w-72 h-72 m-12 rounded-full object-cover border-4 border-primary shadow-xl"
+            />
+            
+
         </div>
-        <div className="max-w-screen flex justify-center text-lg text-primary mt-4 mx-4">
-            Building software. Telling stories. Finding the narratives in both.
-        </div>
-    </div>
-);};
+    );
+};
+
 export default Home
+
