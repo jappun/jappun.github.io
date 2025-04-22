fab623f02e00a6fc25b1e26226bc8a12e4714982
jappun <jappun@student.ubc.ca>
2025-04-21 21:39:34 -0700

    projects: add aiblocker, remove portfolio

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -6,40 +6,18 @@ import insightubc from "../img/insightubc.png"
 import portfolio from "../img/portfolio.png"
 import gather from "../img/gather.png"
 import busbuddies from "../img/busbuddies.png"
+import aiblocker from "../img/aiblocker.png"
+
 
 export const projects = [
   {
-    title: "BusBuddies",
-    tech: ["Next.js", "React", "Tailwind CSS", "Supabase", "Figma"],
+    title: "should i ai?",
+    tech: ["HTML", "CSS", "JavaScript"],
     description:
-    "Matching platform for UBC students to find study buddies on their daily commute. Users with UBC student emails can create a profile, search for matches, and chat with potential buddies.",
-    img: busbuddies,
-      github: "https://github.com/joy1234567891/BusBuddies",
+    "Chrome extension that temporarily blocks AI tools for a user-set time period to motivate intentional AI usage and prevent overreliance.",
+    img: aiblocker,
+      github: "https://github.com/jappun/ai-blocker",
 
-  },
-  {
-    title: "HerVision",
-    tech: ["Swift", "xCode"],
-    description:
-    "HerVision is an iOS application that creates a personalized and social e-commerce experience for Aritzia customers. Users can search Aritzia's inventory, add items to their digital wardrobe, and upload images of their outfits to a communtiy explore page.",
-    img: hervision,
-      github: "https://github.com/jappun/her-vision",
-  },
-  {
-    title: "LexiLog",
-    tech: ["HTML", "CSS", "JavaScript", "Worknik API"],
-    description:
-      "Powered by the Wordnik API, LexiLog is a personal dictionary Chrome Extension. Users can search and save words with their preferred definition and any personal notes.",
-      img: dictionary,
-      github: "https://github.com/jappun/dict-chrome"
-  },
-  {
-    title: "Workout Journal",
-    tech: ["Java", "JSON", "JUnit"],
-    description:
-      "Java GUI where users can track their workouts. Complete with data persistence.",
-      img: workoutjournal,
-      github: "https://github.com/jappun/workout-journal"
   },
   {
     title: "Gather",
@@ -51,6 +29,22 @@ export const projects = [
 
   },
   {
+    title: "Workout Journal",
+    tech: ["Java", "JSON", "JUnit"],
+    description:
+      "Java GUI where users can track their workouts. Complete with data persistence.",
+      img: workoutjournal,
+      github: "https://github.com/jappun/workout-journal"
+  },
+  {
+    title: "HerVision",
+    tech: ["Swift", "xCode"],
+    description:
+    "HerVision is an iOS application that creates a personalized and social e-commerce experience for Aritzia customers. Users can search Aritzia's inventory, add items to their digital wardrobe, and upload images of their outfits to a communtiy explore page.",
+    img: hervision,
+      github: "https://github.com/jappun/her-vision",
+  },
+  {
     title: "Community Centre Database",
     tech: ["SQL", "PHP", "Figma"],
     description:
@@ -59,20 +53,37 @@ export const projects = [
       github: "https://github.com/jappun/community-db",
   },
   {
+    title: "BusBuddies",
+    tech: ["Next.js", "React", "Tailwind CSS", "Supabase", "Figma"],
+    description:
+    "Matching platform for UBC students to find study buddies on their daily commute. Users with UBC student emails can create a profile, search for matches, and chat with potential buddies.",
+    img: busbuddies,
+      github: "https://github.com/joy1234567891/BusBuddies",
+
+  },
+  {
+    title: "LexiLog",
+    tech: ["HTML", "CSS", "JavaScript", "Worknik API"],
+    description:
+      "Powered by the Wordnik API, LexiLog is a personal dictionary Chrome Extension. Users can search and save words with their preferred definition and any personal notes.",
+      img: dictionary,
+      github: "https://github.com/jappun/dict-chrome"
+  },
+  {
     title: "InsightUBC",
     tech: ["TypeScript", "JavaScript", "React", "Postman"],
     description:
       "Fullstack web application where users can filter through historical UBC course data. This was built for a UBC course, so I cannot share the code. Demo video coming soon!",
       img: insightubc,
   },
-  {
-    title: "Portfolio",
-    tech: ["JavaScript", "Tailwind CSS", "React", "Vite"],
-    description:
-      "The website you're on now!",
-      img: portfolio,
-      github: "https://github.com/jappun/jappun.github.io"
-  },
+  // {
+  //   title: "Portfolio",
+  //   tech: ["JavaScript", "Tailwind CSS", "React", "Vite"],
+  //   description:
+  //     "The website you're on now!",
+  //     img: portfolio,
+  //     github: "https://github.com/jappun/jappun.github.io"
+  // },
 ];
 
 export default projects;
